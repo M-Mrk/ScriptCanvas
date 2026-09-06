eafbaa4f202a9d90121d00cc9d761858e946c4ee
Matze Murek <matze.murek@gmail.com>
2026-09-06 03:32:57 +0200

    add tutorial box and related methods

--- web/src/ts/tutorial.ts
+++ web/src/ts/tutorial.ts
@@ -1,37 +1,52 @@
 import { debounce, get_element } from "./common"
 
-interface TutorialSteps {
+enum BoxPosition {
+  Top = "top",
+  Right = "right",
+  Bottom = "bottom",
+  Left = "left",
+}
+
+interface TutorialStep {
   target: string,
   text: string,
+  box: BoxPosition,
 }
-const steps: TutorialSteps[] = [
+const steps: TutorialStep[] = [
   {
     target: "#editor-container",
-    text: "This is the editor. In here you write your script. It uses the monaco editor, which you might know from VSCode, so it uses the same keybinds."
+    text: "This is the editor. In here you write your script. It uses the monaco editor, which you might know from VSCode, so it uses the same keybinds.",
+    box: BoxPosition.Right,
   },
   {
     target: "#output-container",
-    text: "This is the output. There you can see the result of your script."
+    text: "This is the output. There you can see the result of your script.",
+    box: BoxPosition.Left,
   },
   {
     target: "#top-settings",
-    text: "Here you can switch what language and output to (More comming soon!). By default it uses Rhai, a scripting language related to JavaScript and Rust, and outputs as a grid, meaning that your script gets run over each pixel in a grid and returns that pixels RGB value. So a script just returning '[255, 255, 255]' would create a white image."
+    text: "Here you can switch what language and output to (More comming soon!). By default it uses Rhai, a scripting language related to JavaScript and Rust, and outputs as a grid, meaning that your script gets run over each pixel in a grid and returns that pixels RGB value. So a script just returning '[255, 255, 255]' would create a white image.",
+    box: BoxPosition.Bottom,
   },
   {
     target: "#run-btn",
-    text: "Press here to run your script"
+    text: "Press here to run your script",
+    box: BoxPosition.Bottom,
   },
   {
     target: "#middle-icon",
-    text: "Or here"
+    text: "Or here",
+    box: BoxPosition.Bottom,
   },
   {
     target: "#console",
-    text: "If you're ever stuck on something you can use debug and print statements in your script and they will be shown here. For rhai those are 'print()' and 'debug()'."
+    text: "If you're ever stuck on something you can use debug and print statements in your script and they will be shown here. For rhai those are 'print()' and 'debug()'.",
+    box: BoxPosition.Top,
   },
   {
     target: "#help-btn",
-    text: "Thats all for now! But you can always revisit this tutorial under this menu. Now go ahead and write some logic and see how it results into an image."
+    text: "Thats all for now! But you can always revisit this tutorial under this menu. Now go ahead and write some logic and see how it results into an image.",
+    box: BoxPosition.Bottom,
   }
 ]
 
@@ -104,7 +119,27 @@ const show_overlay = (target: string) => {
   ov_left.style.bottom = `${viewport_height - target_pos.bottom}px`;
 }
 
-let last_target_i = 0;
+const show_box = (step: TutorialStep) => {
+  const box = get_element('#tutorial-box');
+  box.classList.add('shown');
+  box.innerText = step.text;
+
+  const options = [BoxPosition.Top, BoxPosition.Right, BoxPosition.Bottom, BoxPosition.Left];
+  for (let opt of options) {
+    box.classList.remove(opt);
+  }
+
+  box.classList.remove('left');
+  box.classList.add(step.box);
+}
+
+const show_step = (step: TutorialStep) => {
+  show_border(step.target);
+  show_overlay(step.target);
+  show_box(step);
+}
+
+let last_step = 0;
 export const init_tutorial = () => {
   const target_observer = new ResizeObserver(() => {
     if (last_target) {
@@ -114,11 +149,9 @@ export const init_tutorial = () => {
   });
   target_observer.observe(document.documentElement);
 
-  show_border(steps[0]?.target as string);
-  show_overlay(steps[0]?.target as string);
+  show_step(steps[0] as TutorialStep)
   setInterval(() => {
-    last_target_i = (last_target_i + 1) % steps.length;
-    show_border(steps[last_target_i]?.target as string);
-    show_overlay(steps[last_target_i]?.target as string);
+    last_step = (last_step + 1) % steps.length;
+    show_step(steps[last_step] as TutorialStep)
   }, 1500);
 }
